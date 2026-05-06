14ecebf323a73d5f9de296bc54d3703be0cf0358
andreerfr <andreerci@gmail.com>
2026-05-06 10:48:44 -0300

    proximo do final

--- aula 11- mais js/script.js
+++ aula 11- mais js/script.js
@@ -6,7 +6,12 @@ function botao1(){
  //  window.document.write("EU NÃO ACREDITO");
    // document.write("<br>Pior que é verdade");
    // element.innerHTML = "Novo Texto";
-   const element = document.getElementById("p01");
-   let texto = document.getElementById("input1").value;
-   element.innerHTML = "<b>Você digitou: </b>" + texto;
+  // const element = document.getElementById("p01");
+ //  let texto = document.getElementById("input1").value;
+  // element.innerHTML = "<b>Você digitou: </b>" + texto;
 }
+  function converterCelsiusParaF() {
+    const celsius = document.getElementById("celsius").value;
+    const graus_f = document.getElementById("graus_f");
+    graus_f.innerHTML = ((9 * celsius +160) /5)+ "F";
+  }
